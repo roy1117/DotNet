1ffda9cec32d278a5d24a0c96ebbcff1dcbeff28
roy1117 <roy.choi@edwardsvacuum.com>
2023-08-23 19:02:22 +0900

    Added files

---
+++ Wix/Command_Line_Project/WinTutorial/ConsoleProgramme/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleProgramme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String message;
+            Console.WriteLine("Hello World!");
+            message = Console.ReadLine();
+        }
+    }
+}
